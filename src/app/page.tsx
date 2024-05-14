93672f9d8a1b560a15b0dfb219d4bcb70413b773
MPires <moysespires@outlook.pt>
2024-05-14 14:29:17 -0300

    feat: add initial search-input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Header from "./components/Header/Header";
+import SearchInput from "./components/Inputs/SearchInput/SearchInput";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <SearchInput />
+      </div>
+    </>
+  );
 };
 
 export default Home;
